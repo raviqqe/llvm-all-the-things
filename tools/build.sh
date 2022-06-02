399596d62aa207b30e4538b2e627a63a2d766be8
Yota Toyama <raviqqe@gmail.com>
2022-06-02 15:58:53 +0000

    Try disabling lld

--- tools/build.sh
+++ tools/build.sh
@@ -19,14 +19,11 @@ cd build
 
 cmake ../$repository/llvm \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_COMPILER=clang \
-  -DCMAKE_CXX_COMPILER=clang++ \
   -DCMAKE_INSTALL_PREFIX=/tmp/llvm \
   -DLLVM_ENABLE_PROJECTS='bolt;clang;clang-tools-extra;flang;lld;lldb;mlir;polly' \
   -DLLVM_ENABLE_RUNTIMES='compiler-rt;libc;libcxx;libcxxabi;libunwind' \
   -DLLVM_PARALLEL_LINK_JOBS=1 \
   -DLLVM_POLLY_LINK_INTO_TOOLS=ON \
-  -DLLVM_TARGETS_TO_BUILD=all \
-  -DLLVM_USE_LINKER=lld
+  -DLLVM_TARGETS_TO_BUILD=all
 
 cmake --build . --target install
