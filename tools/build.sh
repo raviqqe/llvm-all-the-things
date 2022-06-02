c4c3c2da5b2d4a26d04acec86af6bc6c949a707b
Yota Toyama <raviqqe@gmail.com>
2022-06-02 01:43:47 +0000

    Try build

--- tools/build.sh
+++ tools/build.sh
@@ -5,3 +5,8 @@ set -e
 version=14.0.4
 
 git clone --depth 1 -b llvmorg-$version https://github.com/llvm/llvm-project
+
+cmake -G Ninja .. \
+  -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;lld;lldb;mlir;polly' \
+  -DLLVM_ENABLE_RUNTIMES='compiler-rt;libcxx;libcxxabi;lib;libunwind' \
+  -DLLVM_POLLY_LINK_INTO_TOOLS=ON
