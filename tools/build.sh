06654afb7eb67e6e2742772fabe0f5a9ac76e7a7
Yota Toyama <raviqqe@gmail.com>
2022-06-02 15:38:34 +0000

    Limit link jobs

--- tools/build.sh
+++ tools/build.sh
@@ -20,6 +20,7 @@ cd build
 cmake ../$repository/llvm \
   -DLLVM_ENABLE_PROJECTS='bolt;clang;clang-tools-extra;flang;lld;lldb;mlir;polly' \
   -DLLVM_ENABLE_RUNTIMES='compiler-rt;libc;libcxx;libcxxabi;libunwind' \
+  -DLLVM_PARALLEL_LINK_JOBS=1 \
   -DLLVM_POLLY_LINK_INTO_TOOLS=ON \
   -DLLVM_TARGETS_TO_BUILD=all \
   -DLLVM_USE_LINKER=lld
