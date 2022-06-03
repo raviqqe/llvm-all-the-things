5e093d6c608c4459f163b25836e9026c2ac7bd56
Yota Toyama <raviqqe@gmail.com>
2022-06-03 13:52:28 +0000

    Archive llvm

--- tools/build.sh
+++ tools/build.sh
@@ -4,6 +4,7 @@ set -e
 
 tag=llvmorg-14.0.4
 repository=llvm-project
+directory=/tmp/llvm
 
 if ! [ -d $repository ]; then
   git clone --depth 1 -b $tag https://github.com/llvm/$repository
@@ -20,9 +21,11 @@ cd build
 cmake ../$repository/llvm \
   -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX=/tmp/llvm \
+  -DCMAKE_INSTALL_PREFIX=$directory \
   -DLLVM_ENABLE_PROJECTS=mlir \
   -DLLVM_PARALLEL_LINK_JOBS=1 \
   -DLLVM_TARGETS_TO_BUILD=all
 
 cmake --build . --target install
+
+tar xf llvm.tar.xz -C $directory .
