1f110401441b1c030b13c2462c290a848823d527
Yota Toyama <raviqqe@gmail.com>
2022-06-03 14:03:33 +0000

    Fix directory

--- tools/build.sh
+++ tools/build.sh
@@ -16,16 +16,19 @@ else
 fi
 
 mkdir -p build
-cd build
 
-cmake ../$repository/llvm \
-  -G Ninja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX=$directory \
-  -DLLVM_ENABLE_PROJECTS=mlir \
-  -DLLVM_PARALLEL_LINK_JOBS=1 \
-  -DLLVM_TARGETS_TO_BUILD=all
+(
+  cd build
 
-cmake --build . --target install
+  cmake ../$repository/llvm \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$directory \
+    -DLLVM_ENABLE_PROJECTS=mlir \
+    -DLLVM_PARALLEL_LINK_JOBS=1 \
+    -DLLVM_TARGETS_TO_BUILD=all
+
+  cmake --build . --target install
+)
 
 tar cf llvm.tar.xz -C $directory .
