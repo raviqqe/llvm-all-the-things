d24845ff3821d1d471b9a33d8c98ddfba7d5d649
Yota Toyama <raviqqe@gmail.com>
2022-06-02 21:30:33 -0700

    Initialize project (#1)
    * Add build script
    
    * Fix tag
    
    * Add build workflow
    
    * Update readme
    
    * Try build
    
    * Ignore llvm-project directory
    
    * Fix more
    
    * Fix
    
    * Fix more
    
    * Build it
    
    * Install it
    
    * Change job name
    
    * Run it also on macOS
    
    * Build all targets
    
    * Use lld
    
    * Limit link jobs
    
    * Install llvm first
    
    * Use clang
    
    * Set install prefix
    
    * Try disabling lld
    
    * Make it minimum
    
    * Use Ninja
    
    * Disable polly
    
    * Install ninja

---
+++ tools/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+tag=llvmorg-14.0.4
+repository=llvm-project
+
+if ! [ -d $repository ]; then
+  git clone --depth 1 -b $tag https://github.com/llvm/$repository
+else
+  (
+    cd $repository
+    git checkout $tag
+  )
+fi
+
+mkdir -p build
+cd build
+
+cmake ../$repository/llvm \
+  -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/tmp/llvm \
+  -DLLVM_ENABLE_PROJECTS=mlir \
+  -DLLVM_PARALLEL_LINK_JOBS=1 \
+  -DLLVM_TARGETS_TO_BUILD=all
+
+cmake --build . --target install
