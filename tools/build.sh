c1b0142f86cde576b2efd6b97ed29a1213e648e4
Yota Toyama <raviqqe@gmail.com>
2022-06-03 01:39:10 +0000

    Make it minimum

--- tools/build.sh
+++ tools/build.sh
@@ -20,8 +20,7 @@ cd build
 cmake ../$repository/llvm \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=/tmp/llvm \
-  -DLLVM_ENABLE_PROJECTS='bolt;clang;clang-tools-extra;flang;lld;lldb;mlir;polly' \
-  -DLLVM_ENABLE_RUNTIMES='compiler-rt;libc;libcxx;libcxxabi;libunwind' \
+  -DLLVM_ENABLE_PROJECTS='mlir;polly' \
   -DLLVM_PARALLEL_LINK_JOBS=1 \
   -DLLVM_POLLY_LINK_INTO_TOOLS=ON \
   -DLLVM_TARGETS_TO_BUILD=all
